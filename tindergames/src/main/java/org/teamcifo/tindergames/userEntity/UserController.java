9ced4b3ab94b27fb6113a608c44c0db64fa1ef8d
ProtossGP32 <19353116+ProtossGP32@users.noreply.github.com>
2023-03-11 18:48:16 +0100

    feat: Several improvements in User management: - Unified User creation and update into a single HTML form - Added Details button for each user, that gives access to Update and Delete that user - Documented every Controller method - Polished redirections between methods

--- tindergames/src/main/java/org/teamcifo/tindergames/userEntity/UserController.java
+++ tindergames/src/main/java/org/teamcifo/tindergames/userEntity/UserController.java
@@ -2,7 +2,7 @@ package org.teamcifo.tindergames.userEntity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,20 +17,40 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    // Index page
+    /**
+     * Users controller main page
+     * @param containerToView contains relevant data to correctly render thymeleaf view
+     * @return the main page HTML file
+     */
     @RequestMapping({"/", ""})
-    public String index(ModelMap containerToView) {
+    public String index(Model containerToView) {
         // Retrieve all available users
         containerToView.addAttribute("usersFromController", userService.getAllUsers());
-        //containerToView.addAttribute("responseMessage", null);
         return "users/index";
     }
 
+    // CRUD operations
+    // - Create a new user:
+    /**
+     * User's creation GET method. It initializes a new user so it can be populated
+     * @param containerToView contains the new empty User
+     * @return the user's creation form HTML file
+     */
     @GetMapping(value = {"/createUser", "/createUser/"})
-    public String createUser(ModelMap containerToView) {
-        containerToView.addAttribute("newUser", new User());
-        return "users/createUser";
+    public String createUser(Model containerToView) {
+        containerToView.addAttribute("user", new User());
+        containerToView.addAttribute("operation", "createUser");
+        return "users/userForm";
     }
 
+    /**
+     * User's creation POST method. Based on the given ID, if it doesn't exist, the newUser object is stored in the DB
+     * @param id is the ID of the new User
+     * @param newUser is the new User object
+     * @param redirectAttributes contains the response message to be rendered in the GET method when finished
+     * @return a redirect to the GET method
+     */
     @PostMapping(value = "/createUser/{id}")
     public String createUser(@PathVariable("id") String id, Optional<User> newUser, RedirectAttributes redirectAttributes) {
         // Check that the ID is not already used inside the DB
@@ -38,14 +58,26 @@ public class UserController {
             redirectAttributes.addFlashAttribute("responseMessage", "UserID " + id + " already used! Try again");
         }
         else {
-            userService.addUserToDB(newUser.get());
-            redirectAttributes.addFlashAttribute("responseMessage", "User " + newUser.get().getUsername() + " saved");
+            // Check that path variable ID equals the ID of the new user
+            if (newUser.isPresent() && id.equals(newUser.get().getUserId())) {
+                userService.addUserToDB(newUser.get());
+                redirectAttributes.addFlashAttribute("responseMessage", "User " + newUser.get().getUsername() + " saved");
+            } else {
+                redirectAttributes.addFlashAttribute("responseMessage", "Received object doesn't contain a user or something wrong with the provided IDs!");
+            }
         }
-        return "redirect:";
+        return "redirect:/users/createUser";
     }
 
-    @GetMapping(value = "/user/{id}")
-    public String getUserByID(@PathVariable("id") String id, ModelMap containerToView) {
+    // - Read an existing user by ID
+    /**
+     * Retrieve the User object based on the provided ID
+     * @param id is the ID of the desired User
+     * @param containerToView contains the User data from the DB as well as the response message
+     * @return the path to the User Details HTML file
+     */
+    @GetMapping(value = "/id/{id}")
+    public String getUserByID(@PathVariable("id") String id, Model containerToView) {
         User userFromDB = userService.getUserByID(id);
         containerToView.addAttribute("user", userFromDB);
         if (userFromDB != null) {
@@ -56,8 +88,15 @@ public class UserController {
         return "users/userDetails";
     }
 
-    @GetMapping(value = "/user/{username}")
-    public String getUserByUsername(@PathVariable("username") String username, ModelMap containerToView) {
+    // - Read an existing user by username
+    /**
+     * Retrieve the User object based on the provided username
+     * @param username is the username of the desired User
+     * @param containerToView contains the User data from the DB as well as the response message
+     * @return the path to the User Details HTML file
+     */
+    @GetMapping(value = "/username/{username}")
+    public String getUserByUsername(@PathVariable("username") String username, Model containerToView) {
         User userFromDB = userService.getUserByUsername(username);
         containerToView.addAttribute("user", userFromDB);
         if (userFromDB != null) {
@@ -68,36 +107,68 @@ public class UserController {
         return "users/userDetails";
     }
 
+    // - Update an existing user by ID
+    /**
+     * User's update GET method. Based on the provided ID, if the user exists, it will be retrieved and presented in a
+     * form for further modification.
+     * @param id is the user-to-update ID
+     * @param containerToView contains the User object that matches the given ID
+     * @param redirectAttributes contain the response message of the operation
+     * @return the path to the User update form if the user exists, or a redirect to the User's main page otherwise
+     */
     @GetMapping(value = "/updateUser/{id}")
-    public String updateUser(@PathVariable("id") String id, ModelMap containerToView, RedirectAttributes redirectAttributes) {
+    public String updateUser(@PathVariable("id") String id, Model containerToView, RedirectAttributes redirectAttributes) {
         // Retrieve the user based on the provided ID
         User userFromDB = userService.getUserByID(id);
         if (userFromDB != null) {
+            // Store the user object and go to the update form
             containerToView.addAttribute("user", userFromDB);
-            return "users/userUpdate";
+            containerToView.addAttribute("operation", "updateUser");
+            return "users/userForm";
         } else {
+            // Redirect the user to the main page
             redirectAttributes.addFlashAttribute("responseMessage", "User ID " + id + " not found!");
             return "redirect:/users/";
         }
     }
 
+    /**
+     * User's update POST method. Based on the provided ID, if the user exists into the DB, it is replaced
+     * with the updated one
+     * @param id is the ID of the user to update
+     * @param updatedUser is the User object with the updated values
+     * @param redirectAttr contains the response message and the updated user object for further view rendering
+     * @return the path to the GET method
+     */
     @PostMapping(value = "/updateUser/{id}")
-    public String updateUser(@PathVariable("id") String id, Optional<User> updatedUser, ModelMap containerToView) {
+    public String updateUser(@PathVariable("id") String id, Optional<User> updatedUser, RedirectAttributes redirectAttr) {
         // Retrieve the user based on the provided ID
         User userToUpdate = userService.getUserByID(id);
-        // Check if the User already exists and the IDs are equal
-        if (userToUpdate != null && updatedUser.get().getUserId().equals(userToUpdate.getUserId())) {
-            // Update the user
-            userService.addUserToDB(updatedUser.get());
-            // Return response message
-            containerToView.addAttribute("responseMessage", "User " + updatedUser.get().getUsername() + " updated");
+        if (updatedUser.isPresent()) {
+            // Check if the User already exists and the IDs are equal
+            if (userToUpdate != null && updatedUser.get().getUserId().equals(userToUpdate.getUserId())) {
+                // Update the user
+                userService.updateUserInDB(updatedUser.get());
+                // Return response message
+                redirectAttr.addFlashAttribute("responseMessage", "User " + updatedUser.get().getUsername() + " updated");
+            } else {
+                // Something wrong with the ID or the Database
+                redirectAttr.addFlashAttribute("responseMessage", "UserID " + id + " not found or doesn't match the DB");
+            }
         } else {
-            containerToView.addAttribute("updatedUser", updatedUser.get());
-            containerToView.addAttribute("responseMessage", "UserID " + id + " not found or doesn't match the DB");
+            // Something wrong with the updated object
+            redirectAttr.addFlashAttribute("responseMessage", "Received object doesn't contain a user!");
         }
-        return "/updateUser/" + id;
+        // Redirect to the GET method
+        return "redirect:/users/updateUser/" + id;
     }
 
+    /**
+     * User's deletion method based on its ID. If the ID exists, the user is removed from the DB
+     * @param id is the ID of the user to delete
+     * @param redirectAttributes include the response message of the operation
+     * @return redirects to the user's main page to show the updated list of users
+     */
     @GetMapping("/deleteUser/{id}")
     public String deleteUser(@PathVariable("id") String id, RedirectAttributes redirectAttributes) {
         // Retrieve the user based on the provided ID
